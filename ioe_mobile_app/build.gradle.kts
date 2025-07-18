91f6329cdb9ac495610eddc2d476ecfc0ef2fd84
Sagarnp <114459598+Sagarnp1@users.noreply.github.com>
2025-07-18 17:42:39 +0545

    Add files via upload

---
+++ ioe_mobile_app/build.gradle.kts
@@ -0,0 +1,36 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+
+
+//rootProject.layout.buildDirectory.set(layout.buildDirectory.dir("build"))
+
+subprojects {
+
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+
+
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
+
+
+
+
+
+
+
+
+
+
